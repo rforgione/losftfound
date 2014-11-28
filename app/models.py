3664d247b59b888fd803e10fe3df69d95a0a350a
Robert Forgione <rforgione@gmail.com>
2014-11-27 22:24:51 -0500

    Added Item models

--- app/models.py
+++ app/models.py
@@ -5,6 +5,20 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	nickname = db.Column(db.String(64), index=True, unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
+	items = db.relationship('Item', backref='owner', lazy='dynamic')
 
 	def __repr__(self):
 		return '<User %r>' % (self.nickname)
+
+class Item(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	body = db.Column(db.String(140))
+	description = db.Column(db.String(140))
+	location_name = db.Column(db.String(64))
+	location_address = db.Column(db.String(64))
+	message = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return '<Item %r>' % (self.body)
